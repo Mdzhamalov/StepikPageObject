a27893986a8004ae0f0da2e80bb21fa68edb8114
Memet Dzhamalov <memet.dzhamalov@exinity.com>
2024-03-07 17:13:26 +0400

    add new test unit4_module3_step4

--- pages/product_page.py
+++ pages/product_page.py
@@ -14,7 +14,7 @@ class ProductPage(BasePage):
         self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET).click()
 
     def should_be_product_in_the_basket(self):
-        # self.should_be_product_title()
+        self.should_be_product_title()
         self.basket_price_match()
 
     def should_be_product_title(self):
