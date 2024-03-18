a6328fad243297d67bbcd7e90c6d087e8ecfc775
Memet Dzhamalov <memet.dzhamalov@exinity.com>
2024-03-18 18:19:33 +0400

    add new test to product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -2,9 +2,9 @@ import pytest
 
 from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 
 
-@pytest.mark.skip
 @pytest.mark.parametrize('promocode', ["0", "1", "2", "3", "4", "5", "6",
                                        pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, promocode):
@@ -17,7 +17,6 @@ def test_guest_can_add_product_to_basket(browser, promocode):
     product_page.should_be_product_in_the_basket()
 
 
-@pytest.mark.skip
 def test_gest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
     page = MainPage(browser, link)
@@ -27,7 +26,6 @@ def test_gest_cant_see_success_message_after_adding_product_to_basket(browser):
     product_page.should_not_be_success_message()
 
 
-@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
     page = MainPage(browser, link)
@@ -36,7 +34,6 @@ def test_guest_cant_see_success_message(browser):
     product_page.should_not_be_success_message()
 
 
-@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
     page = MainPage(browser, link)
@@ -58,4 +55,15 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+@pytest.mark.new
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, link)
+    basket_page.should_be_message_of_empty_basket()
+    basket_page.should_be_empty_basket()
 
